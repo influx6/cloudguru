f09389c7f5599bbec9d32ad94ec2a3e10e684d23
Alexander Ewetumo <trinoxf@gmail.com>
2019-12-28 22:58:00 +0800

    UPT: Adding go modules

---
+++ cmd/rewrite/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+   "github.com/influx6/cloudguru/pkg/rewrite"
+)
+
+func main() {
+
+}
---
+++ pkg/rewrite/rewrite.go
@@ -0,0 +1,9 @@
+package rewrite
+
+import (
+    "github.com/PuerkitoBio/goquery"
+)
+
+func getPage(pageURI string) (goquery.Document, error) {
+
+}
